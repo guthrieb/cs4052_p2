2c03ec4bbd5905f757877c75099911e8ffeca6aa
jg229 <joeguthrie2@hotmail.com>
2018-11-14 11:25:28 +0000

    initial commit

--- src/main/java/modelChecker/Main.java
+++ src/main/java/modelChecker/Main.java
@@ -1,23 +1,33 @@
 package modelChecker;
 
 import formula.FormulaParser;
+import formula.stateFormula.AtomicProp;
 import formula.stateFormula.StateFormula;
 import model.Model;
+import model.State;
+import model.Transition;
 
 import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
+
+    private static final String QUERY_FILEPATH = "src/test/resources/ctl1";
+    private static final String MODEL_FILEPATH = "src/test/resources/model1.json";
+    private static final String CONSTRAINT_FILE_PATH = "src/test/resources/constraint1";
+
     public static void main(String[] args) {
         try {
-            StateFormula formula = FormulaParser.parseRawFormulaString("AG p");
-            Model model = Model.parseModel("src/test/resources/model1.json");
-            System.out.println(model);
-            System.out.println(Arrays.toString(model.getTransitions()));
-            System.out.println(Arrays.toString(model.getStates()));
+            FormulaParser constraintParser = new FormulaParser(CONSTRAINT_FILE_PATH);
+            FormulaParser queryParser = new FormulaParser(QUERY_FILEPATH);
+            Model model = Model.parseModel(MODEL_FILEPATH);
+            StateFormula query = queryParser.parse();
+            StateFormula constraint = constraintParser.parse();
 
 
-//            System.out.println(formula);
+            SimpleModelChecker simpleModelChecker = new SimpleModelChecker();
+            simpleModelChecker.check(model, query, constraint);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
