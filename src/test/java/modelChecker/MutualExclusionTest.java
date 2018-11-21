6ffb51243db3f4eb320ad43c3b793dc02c59d8b8
sm347 <sarah_f_macneill@yahoo.co.uk>
2018-11-21 20:44:52 +0000

    Added mutual exclusion tests

---
+++ src/test/java/modelChecker/MutualExclusionTest.java
@@ -0,0 +1,57 @@
+package modelChecker;
+
+import formula.FormulaParser;
+import formula.stateFormula.StateFormula;
+import model.Model;
+import modelChecker.graphbuilding.InvalidTracingException;
+import modelChecker.tracing.InvalidStateFormula;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class MutualExclusionTest {
+
+    @Test
+    public void mutualExclusionCriticalSectionTest() throws IOException, InvalidStateFormula, InvalidTracingException {
+        StateFormula aTrue = FormulaParser.parseRawFormulaString("TRUE");
+
+        FormulaParser parser = new FormulaParser("src/test/resources/mutual-exclusion-test-1.json");
+
+        SimpleModelChecker simpleModelChecker = new SimpleModelChecker();
+
+        boolean check = simpleModelChecker.check(Model.parseModel("src/test/resources/mutual-exclusion.json"), aTrue, parser.parse());
+
+        if (!check) {
+            String[] trace = simpleModelChecker.getTrace();
+
+            System.out.println("MODEL DOES NOT HOLD, EXAMPLE TRACE: " + Arrays.toString(trace));
+        } else {
+            System.out.println("MODEL HOLDS: " + check);
+        }
+
+        Assert.assertTrue(check);
+    }
+
+    @Test
+    public void mutualExclusionReachabilityTest() throws IOException, InvalidStateFormula, InvalidTracingException {
+        StateFormula aTrue = FormulaParser.parseRawFormulaString("TRUE");
+
+        FormulaParser parser = new FormulaParser("src/test/resources/mutual-exclusion-test-2.json");
+
+        SimpleModelChecker simpleModelChecker = new SimpleModelChecker();
+
+        boolean check = simpleModelChecker.check(Model.parseModel("src/test/resources/mutual-exclusion.json"), aTrue, parser.parse());
+
+        if (!check) {
+            String[] trace = simpleModelChecker.getTrace();
+
+            System.out.println("MODEL DOES NOT HOLD, EXAMPLE TRACE: " + Arrays.toString(trace));
+        } else {
+            System.out.println("MODEL HOLDS: " + check);
+        }
+
+        Assert.assertTrue(check);
+    }
+}
